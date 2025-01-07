cec8461871a477ff07bc47758f2ef3255794723f
[Emmanard] <[emmanard9@gmail.com]>
2025-01-08 00:42:07 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,27 @@
+import SearchForm from "../../components/SearchForm";
 
-
-
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
   return (
     <>
-    <h1 className="heading">
-      Home
-    </h1>
-   
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
     </>
-    
-  )
+  );
 }
